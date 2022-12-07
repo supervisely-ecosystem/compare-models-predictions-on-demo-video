1389fcee57a5e24278dc8cbb93c0bc564fde795a
almaz_macbook <almazgimaev@gmail.com>
2022-12-08 02:47:31 +0300

    feat: changed method of merging images

--- src/main.py
+++ src/main.py
@@ -1,9 +1,64 @@
-import stats as s
+from collections import defaultdict
+import os
+from shutil import rmtree
+
+import cv2
+
 import functions as f
+import stats as s
 
 
-print("start")
-f.perform_images_to_videos(s.src_datasets)
-f.collect_videos_to_grid(s.SOURCE_PATH, s.RESULT_PATH, s.min_frames)
-video_info = f.create_project(s.api, s.workspace.id, s.RESULT_PATH)
-print(f"result video uploaded with id {video_info[0]}")
+# get test data
+###############################################################
+project_ids = list(map(int, os.environ["CONTEXT_PROJECTID"].split(",")))
+
+src_datasets = defaultdict(list)
+all_projects_gen = (s.api.dataset.get_list(id) for id in project_ids)
+for ds_list in all_projects_gen:
+    for ds in ds_list:
+        src_datasets[ds.name].append(ds)
+#################################################################
+
+
+try:
+    f.check_datasets(src_datasets)
+except Exception:
+    raise ValueError("Error: current datasets have different structures")
+
+for ds_name, dss in src_datasets.items():
+    os.mkdir(s.TEMP_PATH + "results")
+    RESULT_PATH = os.path.join(s.TEMP_PATH, "results/")
+    result_path = f"{RESULT_PATH}merged.mp4"  # result video path
+    ds_path = os.path.join(s.SOURCE_PATH, ds_name)  # current dataset temp directory
+    grid_size = f.get_grid_size(len(dss))  # get result video grid size
+    images_mat = []
+
+    if ds_name not in os.listdir(s.SOURCE_PATH):
+        os.mkdir(s.SOURCE_PATH + f"{ds_name}/")
+
+    for ds_num, ds in enumerate(dss):
+        ds_images = s.api.image.get_list(ds.id)
+        images_id = []
+        images_path = []
+
+        for num, img in enumerate(ds_images):
+            images_id.append(img.id)
+            if str(num) not in os.listdir(ds_path):
+                os.mkdir(ds_path + f"/{num}")
+            img_path = os.path.join(ds_path, str(num), f"{ds_num}-{img.name}")
+            images_path.append(img_path)
+
+        s.api.image.download_paths(ds.id, images_id, images_path)  # download dataset images
+        
+        orig = [cv2.imread(path) for path in images_path]
+        small = [cv2.resize(img, dsize=(0, 0), fx=0.5, fy=0.5) for img in orig]
+        images_mat.append(small)
+
+    f.create_video_from_images(images_mat, result_path=result_path, grid_size=grid_size)
+
+    f.create_dataset_and_upload_result(s.api, s.project.id, RESULT_PATH)
+    rmtree(ds_path)
+
+# clean temp files
+rmtree(s.TEMP_PATH)
+print("finish")
--- src/stats.py
+++ src/stats.py
@@ -4,14 +4,14 @@ from shutil import rmtree
 from dotenv import load_dotenv
 import supervisely as sly
 
+
 load_dotenv("local.env")
 load_dotenv(".env")
 load_dotenv(os.path.expanduser("~/supervisely/.env"))
 
 api = sly.Api.from_env()
 
-ROOT_PATH = "./src/temp_data/"
-FRAME_RATE = (os.environ["FRAME_RATE"])
+FRAME_RATE = os.environ["FRAME_RATE"]
 workspace_id = os.environ["WORKSPACE_ID"]
 
 workspace = api.workspace.get_info_by_id(workspace_id)
@@ -19,29 +19,19 @@ if workspace is None:
     print("you should put correct workspaceId value to local.env")
     raise ValueError(f"Workspace with id={workspace_id} not found")
 
+project = api.project.create(
+    workspace_id, "compare predictions", sly.ProjectType.VIDEOS, change_name_if_conflict=True
+)
 
-# get test data
+# create temp dir (it will be deleted in the end)
 ###############################################################
-src_project_ids = list(map(int, os.environ["CONTEXT_PROJECTID"].split(",")))
-src_datasets = [api.dataset.get_list(id) for id in src_project_ids]
-# # ###############################################################
-min_frames = 10
+ROOT_PATH = "./src/"
+if "temp_data" in os.listdir(ROOT_PATH):
+    rmtree(ROOT_PATH + "temp_data")
 
-# check projects have same count of datasets
-len_first = len(src_datasets[0]) if src_datasets else None
-same_length = all(len(i) == len_first for i in src_datasets)
-if not same_length:
-    print("Datasets structure differ")
-    raise ValueError("Datasets structure differ")
+os.mkdir(ROOT_PATH + "temp_data")
+TEMP_PATH = os.path.join(ROOT_PATH, "temp_data/")
 
-# create tem dir for dataset images
-if "source" in os.listdir(ROOT_PATH):
-    rmtree(ROOT_PATH + "source")
-if "results" in os.listdir(ROOT_PATH):
-    rmtree(ROOT_PATH + "results")
-
-os.mkdir(ROOT_PATH + "source")
-os.mkdir(ROOT_PATH + "results")
-
-SOURCE_PATH = os.path.join(ROOT_PATH, "source/")
-RESULT_PATH = os.path.join(ROOT_PATH, "results/")
+os.mkdir(TEMP_PATH + "source")
+SOURCE_PATH = os.path.join(TEMP_PATH, "source/")
+###############################################################
