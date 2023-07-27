cc1334584f663b7caf1796e1d940548373d987f5
almaz <almazgimaev@gmail.com>
2023-07-27 13:22:46 +0300

    add main wrapper and test error

--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ import os
 import supervisely as sly
 from supervisely.app.widgets import Container
 
-import src.ui.input as input
-import src.ui.output as output
+
+def main():
+    import src.ui.input as input
+    import src.ui.output as output
 
 
-layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
+    layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
 
-app = sly.Application(layout=layout)
+    app = sly.Application(layout=layout)
+    raise ValueError("test")
+
+if __name__ == "__main__":
+    sly.main_wrapper("main", main)
