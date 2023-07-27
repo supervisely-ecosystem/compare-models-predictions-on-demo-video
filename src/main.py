900a147c366b0aaf594954e920bd34c7c5900afc
almaz <almazgimaev@gmail.com>
2023-07-27 12:56:37 +0300

    return original main, input

--- src/main.py
+++ src/main.py
@@ -3,11 +3,10 @@ import os
 import supervisely as sly
 from supervisely.app.widgets import Container
 
-from src.ui.input import test_card
-# import src.ui.output as output
+import src.ui.input as input
+import src.ui.output as output
 
 
-# layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
-layout = Container(widgets=[test_card])
+layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
 
 app = sly.Application(layout=layout)
