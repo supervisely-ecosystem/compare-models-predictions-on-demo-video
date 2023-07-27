af8f8611d5fc8218e9dfcfc10cf6ef2c804e080e
almaz <almazgimaev@gmail.com>
2023-07-27 13:45:08 +0300

    return original

--- src/main.py
+++ src/main.py
@@ -3,16 +3,10 @@ import os
 import supervisely as sly
 from supervisely.app.widgets import Container
 
-
-def main():
-    import src.ui.input as input
-    import src.ui.output as output
+import src.ui.input as input
+import src.ui.output as output
 
 
-    layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
+layout = Container(widgets=[input.start_card, input.info, output.result_card], gap=15)
 
-    app = sly.Application(layout=layout)
-    raise ValueError("test")
-
-if __name__ == "__main__":
-    sly.main_wrapper("main", main)
+app = sly.Application(layout=layout)
